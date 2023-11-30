075543c142940a1e57a156cbf18f0fe905527bc0
Paddy <paddybalinia@gmail.com>
2023-11-30 07:32:51 -0300

    feat: se agrega favorito

--- static/src/js/Home.js
+++ static/src/js/Home.js
@@ -126,6 +126,37 @@
     spanRemove.classList.add("item__txt");
     spanRemove.textContent = "Eliminar";
 
+    const buttonFavorite = document.createElement("button");
+    buttonFavorite.setAttribute("type", "button");
+    buttonFavorite.setAttribute("aria-label", "Favorite item");
+    buttonFavorite.classList.add("action__favorite");
+
+    const svgFavorite = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    svgFavorite.setAttribute("viewBox", "0 0 24 24");
+    svgFavorite.setAttribute("aria-hidden", "true");
+
+    const pathFavorite = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    pathFavorite.setAttribute(
+      "d",
+      "M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm4.24 16L12 15.45 7.77 18l1.12-4.81-3.73-3.23 4.92-.42L12 5l1.92 4.53 4.92.42-3.73 3.23L16.23 18z"
+    );
+
+    svgFavorite.appendChild(pathFavorite);
+    buttonFavorite.appendChild(svgFavorite);
+
+    const spanFavorite = document.createElement("span");
+    spanFavorite.classList.add("item__txt");
+    spanFavorite.textContent = "Favorito";
+
+    buttonFavorite.appendChild(spanFavorite);
+    divDropdown.appendChild(buttonFavorite);
+
     buttonRemove.appendChild(spanRemove);
     divDropdown.appendChild(buttonRemove);
     buttonEdit.appendChild(spanEdit);
